a141d86ebc891bec5bff00da2e087673e0cd313b
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2012-02-03 12:11:11 -0500

    Cleanup

--- app.js
+++ app.js
@@ -13,16 +13,8 @@ app.configure(function(){
   app.use(express.bodyParser());
 //  app.use(express.logger('dev'))
   app.use(express.methodOverride());
-  app.use(function(req, res, next){
-    var id = /^\/(\d+)\//.exec(req.url) || null;
-    if(id){
-      if(id[1] == null) id[1] = 0;
-      req.url = req.url.slice(id[1].length+1);
-    }
-    next();
-  })
   app.use(app.router);
-  app.use(express.static('/home/diversario/Documents/Projects/IA-PK/'));
+  app.use(express.static('/home'));
 });
 
 app.configure('development', function(){
@@ -33,18 +25,5 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Routes
-
-app.get('/:id', function(req, res, next){
-  var id = req.params.id;
-  
-  if (/^\d+$/.test(id)){
-    req.url += '/';
-    res.redirect(req.url);
-  } else {
-    next();
-  }
-});
-
-app.listen(80, '192.168.0.110');
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3000, '0.0.0.0');
+console.log("Express server listening on port 3000 in %s mode", app.settings.env);
