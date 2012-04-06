5bae5bf0c34a72dae8addf26677e60f2b47e312e
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2012-04-06 15:18:03 -0400

    Using Jade and LESS

--- server/app.js
+++ server/app.js
@@ -4,46 +4,58 @@
 
 var express = require('express')
   , fs = require('fs')
+  , ENV = process.env['ENV'] || 'development'
   , jade = require('jade')
+  , gzip = require('connect-gzip')
+  , less = require('less')
+  , publicDir = __dirname + '/../client'
 ;
 
 
 var app = module.exports = express.createServer();
 
-// Configuration
 
+// Configuration
 app.set('views', __dirname + '/views')
    .set('view options', { 'layout': false })
-   .set('view engine', 'jade');
+   .set('view engine', 'jade')
+;
+
+app.use(express.bodyParser())
+   .use(express.cookieParser())
+   .use(gzip.gzip({ flags: '--best' }))
+;
+
+// LESS compiler
+app.use(less.middleware(
+ {   'src': publicDir
+   , 'dest': publicDir
+   , 'compress': ENV != 'development'
+   , 'debug': ENV == 'development'
+   , 'force': ENV == 'development'
+ }
+));
+
+// First try to serve content from "publicDir"
+app.use(express.static(publicDir));
+
+app.dynamicHelpers({ messages: require('express-messages') });
 
 
- app.use(express.bodyParser())
-    .use(express.cookieParser())
-    .use(gzip.gzip({ flags: '--best' }))
- ;
 
- // LESS compiler
- app.use(less.middleware(
-   {   'src': publicDir
-     , 'dest': publicDir
-     , 'compress': ENV != 'development'
-     , 'debug': ENV == 'development'
-     , 'force': ENV == 'development'
-   }
- ));
+// Routes
 
- app.use(express.static(__dirname + '/../client'));
- app.use(app.router);
-
- app.dynamicHelpers({ messages: require('express-messages') });
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+// Serve index.jade for requests to /
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+
+// For all other requests try serve a view with the "page" name
+app.get('/:page', function (req, res) {
+  res.render(req.params.page);
 });
+
 
 app.listen(3000, '0.0.0.0');
 console.log("Express server listening on port 3000 in %s mode", app.settings.env);
